2540a7a444708d254ff79eb81b319032e7dfd4e8
el mouataz billah fachatti <mouatazbillah97@gmail.com>
2022-03-05 14:21:43 +0100

    add new repo

---
+++ bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__.'/lib/Service/Container.php';
+require_once __DIR__.'/lib/Model/AbstractShip.php';
+require_once __DIR__.'/lib/Model/Ship.php';
+require_once __DIR__.'/lib/Model/RebelShip.php';
+require_once __DIR__.'/lib/Model/BrokenShip.php';
+require_once __DIR__.'/lib/Service/BattleManager.php';
+require_once __DIR__.'/lib/Service/ShipStorageInterface.php';
+require_once __DIR__.'/lib/Service/PdoShipStorage.php';
+require_once __DIR__.'/lib/Service/JsonFileShipStorage.php';
+require_once __DIR__.'/lib/Service/ShipLoader.php';
+require_once __DIR__.'/lib/Model/BattleResult.php';
+
+$configuration = array(
+    'db_dsn'  => 'mysql:host=localhost;dbname=oo_battle',
+    'db_user' => 'root',
+    'db_pass' => null,
+);
+
---
+++ lib/Service/BattleManager.php
@@ -0,0 +1,65 @@
+<?php
+
+class BattleManager
+{
+    /**
+     * Our complex fighting algorithm!
+     *
+     * @return BattleResult
+     */
+    public function battle(AbstractShip $ship1, $ship1Quantity, AbstractShip $ship2, $ship2Quantity)
+    {
+        $ship1Health = $ship1->getStrength() * $ship1Quantity;
+        $ship2Health = $ship2->getStrength() * $ship2Quantity;
+
+        $ship1UsedJediPowers = false;
+        $ship2UsedJediPowers = false;
+        while ($ship1Health > 0 && $ship2Health > 0) {
+            // first, see if we have a rare Jedi hero event!
+            if ($this->didJediDestroyShipUsingTheForce($ship1)) {
+                $ship2Health = 0;
+                $ship1UsedJediPowers = true;
+
+                break;
+            }
+            if ($this->didJediDestroyShipUsingTheForce($ship2)) {
+                $ship1Health = 0;
+                $ship2UsedJediPowers = true;
+
+                break;
+            }
+
+            // now battle them normally
+            $ship1Health = $ship1Health - ($ship2->getWeaponPower() * $ship2Quantity);
+            $ship2Health = $ship2Health - ($ship1->getWeaponPower() * $ship1Quantity);
+        }
+
+        // update the strengths on the ships, so we can show this
+        $ship1->setStrength($ship1Health);
+        $ship2->setStrength($ship2Health);
+
+        if ($ship1Health <= 0 && $ship2Health <= 0) {
+            // they destroyed each other
+            $winningShip = null;
+            $losingShip = null;
+            $usedJediPowers = $ship1UsedJediPowers || $ship2UsedJediPowers;
+        } elseif ($ship1Health <= 0) {
+            $winningShip = $ship2;
+            $losingShip = $ship1;
+            $usedJediPowers = $ship2UsedJediPowers;
+        } else {
+            $winningShip = $ship1;
+            $losingShip = $ship2;
+            $usedJediPowers = $ship1UsedJediPowers;
+        }
+
+        return new BattleResult($usedJediPowers, $winningShip, $losingShip);
+    }
+
+    private function didJediDestroyShipUsingTheForce(AbstractShip $ship)
+    {
+        $jediHeroProbability = $ship->getJediFactor() / 100;
+
+        return mt_rand(1, 100) <= ($jediHeroProbability*100);
+    }
+}
